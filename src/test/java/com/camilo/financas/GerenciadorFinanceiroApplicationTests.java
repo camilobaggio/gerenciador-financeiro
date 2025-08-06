f8f64365b1c081f199b7810857affe2598b2fbdf
Camilo Baggio <baggiocamilo@gmail.com>
2025-08-06 19:52:20 -0300

    feat: projeto inicial com Spring Boot

---
+++ src/test/java/com/camilo/financas/GerenciadorFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camilo.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
