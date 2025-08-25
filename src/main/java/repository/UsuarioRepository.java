042b48969458a68604aff1b5f77a07c086883ed6
Camilo Baggio <baggiocamilo@gmail.com>
2025-08-25 19:13:53 -0300

    feat: adiciona UsuarioRepository

---
+++ src/main/java/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import com.camilo.financas.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+}
