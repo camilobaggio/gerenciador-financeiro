d51e5a4417dbdf3c48bfbd7863469caa87086e82
Camilo Baggio <baggiocamilo@gmail.com>
2025-08-26 21:17:33 -0300

    feat: adicionando os metodos no controller

--- src/main/java/com/camilo/financas/controller/UsuarioController.java
+++ src/main/java/com/camilo/financas/controller/UsuarioController.java
@@ -4,11 +4,10 @@ package com.camilo.financas.controller;
 import com.camilo.financas.model.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.camilo.financas.service.UsuarioService;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -22,6 +21,24 @@ public class UsuarioController {
         usuarioService.salvar(usuario);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/id")
+    public ResponseEntity<Usuario> buscaPorId (UUID id){
+
+        return ResponseEntity.ok(usuarioService.buscaPorId(id).orElseThrow());
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deletaPorId (UUID id){
+        usuarioService.deletar(id);
+        return  ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+        public ResponseEntity<Void> atualizar (@RequestBody Usuario usuario){
+        usuarioService.atualizar(usuario);
+        return ResponseEntity.ok().build();
+        }
 
 
 }
